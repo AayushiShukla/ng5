e80f31a0c84ed74609397921088bc44c02a8771d
Aayushi Shukla Shukla <v-aashu@microsoft.com>
2018-10-15 14:49:13 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+//1. Import
 import { Component } from '@angular/core';
 
+//2. Decorator - Properties that help define the structure of your component
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+//3. Export class component - Where all the logic resides for the application
 export class AppComponent {
   title = 'ng5';
 }
